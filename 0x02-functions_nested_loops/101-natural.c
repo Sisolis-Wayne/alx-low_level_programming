78f3c9dd3cdc7143eb53b6842b32ff36a6ddc816
Akorede Mohammed <mohammedakoredest@gmail.com>
2023-05-24 08:09:55 +0100

    fixed the error I got from the checker on task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
 /**
- * main - a function  that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line.
+ * main - a function  that computes and prints the sum of all the multiples of
+ * 3 or 5 below 1024 (excluded), followed by a new line.
  *
  * Return: 0 (Always Success)
- *
- */
+*/
  
- int main(void)
- {
- 	int m, sum;
+int main(void)
+{
+	int m, sum;
 	for (m = 0; m < 1024; m++)
 	{
 		if (m % 3 == 0 || m % 5 == 0)
@@ -19,4 +19,4 @@
 	}
 	printf("%d\n", sum);
 	return (0);
- }
+}
