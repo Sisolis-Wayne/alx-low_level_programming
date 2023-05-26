85f3b0f0cc04dbcd7321bb1cd6b597379dfbf232
Akorede Mohammed <mohammedakoredest@gmail.com>
2023-05-26 06:08:10 +0100

    solved the fibonacci problem

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- *
- * Return: 0
- */
+* main - a program that prints the first 50 Fibonacci numbers,
+* starting with 1 and 2, followed by a new line.
+* The numbers must be separated by comma, followed by a space ,
+*
+* Return: 0 (Always)
+*/
 
 int main(void)
 {
-	int i, j, k, counter;
+	unsigned long long int i;
+	unsigned long long int t1 = 1, t2 = 2;
+	unsigned long long int nextTerm = t1 + t2;
 
-	i = 1;
-	j = 2;
+	printf("%llu, %llu, ", t1, t2);
 
-	for (counter = 2; counter < 50; ++counter)
+	for (i = 3; i < 50; ++i)
 	{
-		k = i + j;
-		printf("%d, ", k);
-		i = j;
-		j = k;
+		printf("%llu, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+		if (i == 49)
+		{
+			printf("%llu", nextTerm);
+		}
 	}
 
 	printf("\n");
+
 	return (0);
 }
