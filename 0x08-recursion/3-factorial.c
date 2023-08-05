9b94b6d0115047f095b28d3c9ee4d8ad9f3b866c
sisolis wayne <mohammedakoredest@gmail.com>
2023-08-05 05:54:38 +0100

    a function that returns the length of a string.

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,25 @@
 #include "main.h"
 
 /**
- * factorial - - 
+ * factorial - a function that returns the factorial of a given number.
+ *
+ * Return: Always 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+
+	return (0);
+}
