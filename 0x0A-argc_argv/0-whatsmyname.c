39d74af4163c0a72e62c6ce56365a8f9ac6b807e
sisolis wayne <mohammedakoredest@gmail.com>
2023-08-09 12:34:01 +0100

    resolved the failed checks in the program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - write a program that prints its name, followed by
+ * a new line. If you remove the program, it will print the new
+ * name, without having to compile it again.
+ * You should not remove the path before the name of the program
+ *
+ * @argc:  count of arguments
+ * @argv: array of arguments i.e argument vector
+ *
+ * Return: 1
+ */
+
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("This program was called with \"%s\".\n", argv[0]);
