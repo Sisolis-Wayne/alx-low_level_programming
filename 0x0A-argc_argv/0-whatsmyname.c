25bbb42aa68d4bc851e239076daf838b57a0f45c
sisolis wayne <mohammedakoredest@gmail.com>
2023-08-09 13:07:00 +0100

    resolved an error in the 0-whatsmyname file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - write a program that prints its name, followed by
- * a new line. If you remove the program, it will print the new
- * name, without having to compile it again.
+ * main - a program that prints its name, followed by a new line.
+ *
+ * If you rename the program, it will print the new name,
+ * without having to compile it again
+ *
  * You should not remove the path before the name of the program
  *
- * @argc:  count of arguments
- * @argv: array of arguments i.e argument vector
- *
- * Return: 1
+ * Return: Always 0. 
  */
 
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	printf("This program was called with \"%s\".\n", argv[0]);
-
+	printf ("\"%s\".\n",argv[0]);
 	return (0);
 }
