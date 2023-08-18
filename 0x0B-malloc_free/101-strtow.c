a407ae2984b629b123e19a4476f78d99c733ec2a
sisolis wayne <mohammedakoredest@gmail.com>
2023-08-18 09:30:58 +0100

    a function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * ch_free_grid - frees a 2 dimensional array.
+ * ch_free_grid - frees a 2D array.
  * @grid: multidimensional array of char.
  * @height: height of the array.
  *
