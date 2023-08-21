9873f6ecd94690d08f38d5af9e712eed898bfe2c
sisolis wayne <mohammedakoredest@gmail.com>
2023-08-21 14:40:19 +0100

    resolved the error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,18 +21,27 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	char *filler;
+	unsigned int i;
 
 	if ((nmemb == 0) || (size == 0))
 	{
 		return (NULL);
 	}
 
-	arr = malloc(nmemb * sizeof(size));
+	arr = malloc(nmemb * size);
 
-	if (arr == NULL)
+	if (!arr)
 	{
 		return (NULL);
 	}
+
+	filler = arr;
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		filler[i] = 0;
+	}
+
 
 	return (arr);
 }
