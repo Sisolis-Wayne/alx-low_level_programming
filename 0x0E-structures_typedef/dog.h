6570e1990a48cdf75c7e62e62b7bbc0c61d743d2
sisolis wayne <mohammedakoredest@gmail.com>
2023-08-22 12:47:29 +0100

    resolved the error. a struct for dog containing the name, age and the owner of the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - a data structure for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: the struct dog
+ *
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
