a2a0faae305f61ba5f35ffb95528f909b57e57b0
sisolis wayne <mohammedakoredest@gmail.com>
2023-08-22 06:46:52 +0100

    a program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints file name, date,
+ * time, line and standardized output (when the code is
+ * compiled) using predefined macros
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
